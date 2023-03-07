331dc6738052e2e034d59caaa0c73f18aec4c545
Esa Turkulainen <esa.turkulainen@gmail.com>
2023-03-07 17:54:52 +0200

    init rcs boot

---
+++ src/create_subgroup_df.R
@@ -0,0 +1,45 @@
+library(dplyr)
+
+# Load data
+load("~/CRP_enrichment/data/thldata.rdata")
+H2000 <- thldata$h2000
+
+# Remove leftovers
+rm(thldata)
+
+# Rename
+H2000 <- rename(H2000, Ferritin = FERRITIINI, Hb = B_Hb)
+
+# Apply desired filters
+## Women | Menstruating
+subgroup <- H2000 %>%
+    filter(SP2 == 2) %>% # sex: female
+    filter(BD03 == 1 | BD03 == 2) %>% # menstruation: regular or irregular
+    filter(IKA2 >= 18 & IKA2 < 55) # age over 18, under 55
+
+## Women | Non-menstruating
+# subgroup <- H2000 %>%
+#     filter(SP2 == 2) %>% # sex: female
+#     filter(BD03 == 3) %>% # menstruation: no
+#     filter(IKA2 >= 55) # age over 55
+
+## Women | Non-menstruating
+# subgroup <- H2000 %>%
+#     filter(SP2 == 1) %>% # sex: male
+#     filter(IKA2 >= 18) # age over 18
+
+# Apply additional filters to filtered subgroup, e.g. health status
+subgroup_filtered <- subgroup  %>%
+    filter(BMII_PAINO.x >= 50 & BMII_PAINO.x <= 200) %>% # Filter away people <50kg and >200kg
+    filter(IKA2 >= 20 & IKA2 <= 49) %>% # Filter away too young and too old
+    filter(BD07 == 0) %>% # filter out pregnant women
+    filter(BA08 == 0) %>% # filter out people with heart attacks
+    filter(BA09 == 0) %>% # filter out people with angina
+    filter(BA10 == 0) %>% # cardiac insufficiency / heart failure
+    filter(!(BA26 == 1 & ATC_A10A == 1)) %>% # filter out people who are diabetic AND use insulin
+    filter(BA01 < 4) %>% # filter out "Bad" or "Very bad" SRH
+    select(Ferritin, Hb) %>%
+    na.omit()
+
+# Save
+saveRDS(subgroup_filtered, file = "~/who_ferritin_comment/data/h2000_menstr_women_apparently_healthy.rds")
---
+++ src/results_helper_funcs.R
@@ -0,0 +1,104 @@
+# Helpoer functions for ferritin_threshold_results.qmd
+
+find_saddle <- function(fit, start = 0, stop = 50, interval_length = 10, max = TRUE) {
+
+
+    # init values for while loop
+    end <- start + interval_length
+    i <- -1
+    optim <- 9999
+    while ((((end + i) - optim[[1]]) < 0.001) && i <= 40) {
+        i <- i + 1
+        optim <- optimize(Function(fit), interval = c(start + i, end + i), maximum = max)
+    }
+
+    if (i == 41) {
+        # No saddle found between (start, stop)
+        return(list(saddle.exists = F,
+                    x = 0,
+                    y = 0))
+    } else {
+        x <- optim[[1]]
+        y <- optim[[2]]
+        return(list(saddle.exists = T,
+                    x = x,
+                    y = y))
+    }
+}
+
+
+plot_rcs <- function(data, fit, n_knot, zoom = T) {
+
+    pred <- Predict(fit)
+
+    # Check whether we are working with Hb or sTfR
+    # We need this to know if we are maximising or minimizing
+    # when searching for saddle points!
+    response_var_name <- attributes(pred)$info$ylab
+
+    if (zoom == TRUE) {
+        if (response_var_name == "Hb") {
+            ylims <- c(min(pred$yhat) - 10, max(pred$yhat) + 10)
+        } else {
+            ylims <- c(min(pred$yhat) - 1, max(pred$yhat) + 2)
+        }
+    } else {
+        if (response_var_name == "Hb") {
+            ylims <- c(min(data$Hb), max(data$Hb))
+        } else {
+            ylims <- c(min(data$sTfR), max(data$sTfR))
+        }
+
+    }
+
+    # Test if saddle point exists
+    start = 0
+    stop = 50
+    out <- find_saddle(fit, start = start, stop = stop, max = (response_var_name == "Hb"))
+    saddle.exists <- out$saddle.exists
+    inflection_x <- out$x
+    inflection_y <- out$y
+
+    if (saddle.exists) {
+        subtitle <-
+            paste0("Saddle point at: ", round(inflection_x, 2), " μg/L")
+    } else {
+        subtitle <- paste0("No saddle point between ", start, "-", stop, " μg/L.")
+    }
+
+    ggplot(data = data, aes(x = Ferritin, y = Hb)) +
+        geom_point(alpha = 0.1) +
+        geom_line(data = pred,
+                  aes(x = Ferritin, y = yhat),
+                  size = 0.6) +
+        geom_ribbon(
+            data = pred,
+            aes(x = Ferritin, ymin = lower, ymax = upper),
+            alpha = 0.2,
+            inherit.aes = FALSE
+        ) +
+        geom_segment(
+            x = inflection_x - 10,
+            xend = inflection_x + 10,
+            y = inflection_y,
+            yend = inflection_y,
+            size = 0.4,
+            alpha = saddle.exists
+        ) +
+        geom_segment(
+            x = inflection_x,
+            xend = inflection_x,
+            y = min(data$Hb),
+            yend = inflection_y + 30,
+            linetype = "dashed",
+            size = 0.3,
+            alpha = saddle.exists
+        ) +
+        scale_x_continuous(breaks = seq(0, 150, 25),
+                           labels = seq(0, 150, 25)) +
+        xlim(c(0, 150)) +
+        ylim(ylims) +
+        theme_minimal() +
+        labs(title = paste0("Knots: ", n_knot, ifelse(zoom, " (zoomed)", "")),
+             subtitle = subtitle)
+}
